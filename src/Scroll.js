e1405c4ccf87a10100a5d453cbda1b5a751cdb61
mmarsden89 <mattmarsdendev@gmail.com>
2020-10-15 17:06:23 -0700

    Update scroll to hide behind footer

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import ScrollToTop from 'react-scroll-up'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretSquareUp } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import ScrollToTop from "react-scroll-up";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretSquareUp } from "@fortawesome/free-solid-svg-icons";
 
-function Scroll() {
+const Scroll = () => {
   return (
     <ScrollToTop
       showUnder={160}
       duration={1500}
-      easing={'easeInOutCubic'}
-      style={{'transitionDuration': '1s'}}>
-      <FontAwesomeIcon icon={faCaretSquareUp} className="scrollUp"/>
+      easing={"easeInOutCubic"}
+      style={{ transitionDuration: "1s" }}
+    >
+      <FontAwesomeIcon icon={faCaretSquareUp} className="scrollUp" />
     </ScrollToTop>
-)}
+  );
+};
 
-export default Scroll
+export default Scroll;
