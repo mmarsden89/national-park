10528696c0920af238fd0316030dfb28ecf3cf13
mmarsden89 <mattmarsdendev@gmail.com>
2020-10-15 17:05:39 -0700

    Add footer

---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <a href="https://mmarsden89.github.io" target="_blank">
+        Portfolio
+      </a>
+      <p>© Matt Marsden 2020</p>
+      <a
+        href="mailto:mmarsden89@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Email
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
