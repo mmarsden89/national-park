e8a4d80690857fa374ca35ee78a8f32f25ac957d
mmarsden89 <mattmarsdendev@gmail.com>
2020-10-21 21:17:12 -0700

    Updates

--- precache-manifest.042c5bb5ad8823025a99217bf2deb5b8.js
+++ precache-manifest.042c5bb5ad8823025a99217bf2deb5b8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9beb79b6ac30ec3ab90a6d2f265b7095",
+    "revision": "5d1e985793a3542f36ad3304999fdacc",
     "url": "/national-park-prototype/index.html"
   },
   {
-    "revision": "32e24247126d93fd09ee",
-    "url": "/national-park-prototype/static/css/main.a5dd7f1c.chunk.css"
+    "revision": "07ba0fd23d2bf5e9c9a1",
+    "url": "/national-park-prototype/static/css/main.96508f82.chunk.css"
   },
   {
     "revision": "dd7e93664f2231bff569",
     "url": "/national-park-prototype/static/js/2.e5be32c7.chunk.js"
   },
   {
-    "revision": "32e24247126d93fd09ee",
-    "url": "/national-park-prototype/static/js/main.35e4912b.chunk.js"
+    "revision": "07ba0fd23d2bf5e9c9a1",
+    "url": "/national-park-prototype/static/js/main.2c43f066.chunk.js"
   },
   {
     "revision": "39360f159a926d7f9709",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/national-park-prototype/precache-manifest.52f1f860e98763928b47d8eb8ccb8942.js"
+  "/national-park-prototype/precache-manifest.042c5bb5ad8823025a99217bf2deb5b8.js"
 );
 
 self.addEventListener('message', (event) => {
