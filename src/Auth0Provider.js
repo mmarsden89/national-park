9b681362c887bbb751de3e25b881e13f55cf35b7
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-16 23:43:36 -0800

    update to browser url

--- src/Auth0Provider.js
+++ src/Auth0Provider.js
@@ -7,7 +7,7 @@ ReactDOM.render(
   <Auth0Provider
     domain="dev-0r9j-iz3.auth0.com"
     clientId="1HbLQ6nRcscQnN2UY5i79et3CRBQq3zQ"
-    redirectUri="https://mmarsden89.github.io/national-park-prototype/"
+    redirectUri="https://mmarsden89.github.io/national-park/"
   >
     <App />
   </Auth0Provider>,
